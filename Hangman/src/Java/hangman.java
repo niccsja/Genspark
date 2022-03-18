763fe9079a751b8b3de578e7bceccec956e8e11f
Niville <niville.nicc@gmail.com>
2022-03-18 17:06:56 -0400

    Added the ability to restart game

--- Hangman/src/Java/hangman.java
+++ Hangman/src/Java/hangman.java
@@ -7,7 +7,6 @@ public class hangman {
 
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
         Scanner keyboard = new Scanner(System.in);
         String word = "crawl";
@@ -21,24 +20,35 @@ public class hangman {
         display(wrongGuesses, missedLetters);
         System.out.println("This is the missed string "+missedString);
 
+        gamePlay(keyboard, word, guesses, wrongGuesses, missedLetters);
+    }
+
+    private static void gamePlay(Scanner keyboard, String word, List<Character> guesses, int wrongGuesses, List<String> missedLetters) {
         boolean play = true;
-        while(play) {
+
+        while (true) {
             printWordState(word, guesses);
-           if(!getPlayerGuess(keyboard, word, guesses,missedLetters)){
-               wrongGuesses++;
-           }
-            if(printWordState(word, guesses)){
+            if (!getPlayerGuess(keyboard, word, guesses, missedLetters)) {
+                wrongGuesses++;
+            }
+            if (printWordState(word, guesses)) {
                 break;
-            };
+            }
+            ;
             display(wrongGuesses, missedLetters);
         }
-        System.out.println("Yes! The secret word is "+word+ " You have won!");
+        System.out.println("Yes! The secret word is " + word + " You have won!");
         System.out.println("Do you want to play again?(yes or no)");
         String ans = keyboard.nextLine();
-        if(ans.equals("no")) {
+        if (ans.equals("no")) {
             play = false;
+            System.out.println("The game has ended.");
+        } else
+        {
+            guesses.removeAll(guesses);
+            display(wrongGuesses, missedLetters);
+            gamePlay(keyboard, word, guesses, wrongGuesses, missedLetters);
         }
-
     }
 
     private static boolean getPlayerGuess(Scanner keyboard, String word, List<Character> guesses, List<String> missedLetter) {
@@ -59,11 +69,7 @@ public class hangman {
     private static void display(int userGuess, List<String> missed) {
         gameLogic art = new gameLogic();
         System.out.println("HANGMAN");
-       /* System.out.println("+--+\n"+
-                "   |\n"
-                "   |\n"+
-                "   |\n"+
-        "  ===");*/
+
         if(userGuess == 0) {
             System.out.println(art.hangmanArt()[0]);
         } else if(userGuess == 1) {
